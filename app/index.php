f65b21b87298fe985782eae52618476e23af8030
marcoweb <marcocarvalho.web@gmail.com>
2023-08-07 20:50:20 +0000

    Add PDO_MySQL driver

--- app/index.php
+++ app/index.php
@@ -1,2 +1,8 @@
 <?php
-echo phpinfo();
+$db = new \PDO('mysql:host=localhost;dbname=testdb;charset=utf8mb4', 'username', 'password', array(
+
+    \PDO::ATTR_EMULATE_PREPARES => false,
+
+    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+
+));
